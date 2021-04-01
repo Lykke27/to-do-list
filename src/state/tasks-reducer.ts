a2bccf95d5f698de97c09f96300bfab27646a1ac
Lykke27 <usachev97@gmail.com>
2021-04-01 20:23:10 +0300

    add CHANGE-STATUS and CHANGE-TITLE to task-reducer

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,5 +1,5 @@
 import {v1} from "uuid";
-import {TaskStateType, TaskType, TodoListType} from "../App";
+import {TaskStateType, TaskType} from "../App";
 
 type RemoveTaskActionType = {
     type: "REMOVE-TASK",
@@ -12,8 +12,20 @@ type AddTaskActionType = {
     title: string
     todolistId: string
 }
+type changeTaskStatusActionType = {
+    id: string
+    type: "CHANGE-STATUS"
+    todolistId: string
+    isDone: boolean
+}
+type changeTaskTitleActionType = {
+    id: string
+    type: "CHANGE-TITLE"
+    todolistId: string
+    title: string
+}
 
-type ActionsType = RemoveTaskActionType | AddTaskActionType
+type ActionsType = RemoveTaskActionType | AddTaskActionType | changeTaskStatusActionType | changeTaskTitleActionType
 
 export const tasksReducer = (state: TaskStateType, action: ActionsType) => {
     switch (action.type) {
@@ -30,12 +42,44 @@ export const tasksReducer = (state: TaskStateType, action: ActionsType) => {
                 isDone: false
             }
             let copyState = {...state}
-            copyState[action.todolistId] = [newTask,...state[action.todolistId]]
+            copyState[action.todolistId] = [newTask, ...state[action.todolistId]]
             return copyState
         }
 
+        case "CHANGE-STATUS": {
+            // const todoListTasks = tasks[todoListID]
+            // const task = todoListTasks.find(t => t.id === taskID)
+            // if (task) {
+            //     task.isDone = newIsDoneValue
+            //     setTasks({...tasks}) //складываем таски в объект
+            // }
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(task => {
+                    if (task.id === action.id) {
+                        return {...task, isDone:action.isDone}
+                    } else {
+                        return task
+                    }
+                })
+            }
+        }
+        case "CHANGE-TITLE": {
+            //ПОПРОБОВАТЬ С FIND
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].map(task => {
+                    if (task.id === action.id) {
+                        return {...task, title:action.title}
+                    } else {
+                        return task
+                    }
+                })
+            }
+        }
+
         default:
-            throw new Error("WTF")
+            return state
     }
 }
 
@@ -44,4 +88,10 @@ export const removeTaskAC = (taskId: string, todolistId: string): RemoveTaskActi
 }
 export const addTaskAC = (title: string, todolistId: string): AddTaskActionType => {
     return {type: "ADD-TASK", title, todolistId}
+}
+export const changeTaskStatusAC = (id: string, isDone: boolean, todolistId: string): changeTaskStatusActionType => {
+    return {type: "CHANGE-STATUS", id, todolistId, isDone}
+}
+export const changeTaskTitleAC = (id: string, title: string, todolistId: string): changeTaskTitleActionType => {
+    return {type: "CHANGE-TITLE", id, todolistId, title}
 }
